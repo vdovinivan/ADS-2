17ed551ec592fd906c16edc7bb281099b1c0058c
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-03-05 19:15:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,79 +3,52 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-  
- double b=value;
-  while(b!=1){ 
-    
-    value=value*b;
-    n--;
-  }
-  
-  return value;
-  
+    if (n == 1) {
+        return value;
+    }
+    return value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-  
- if (n<0) 
-   return 0; 
-  
-  if (n==0)
-    return 1;
-  
-  return n*fact(n-1);
+    if (n < 0)
+        return 0;
+    if (n == 0)
+        return 1;
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  
-  double y=0;
-   y=pown(x, n) / fact(n);
-  return y;
-  
+    double y = 0;
+    y = pown(x, n) / fact(n);
+    return y;
 }
 
 double expn(double x, uint16_t count) {
-  
-  double sum = 0;
-  
- while(count!=0){ 
-   
-    sum=sum+calcItem(x, count);
-    count--;
-  }
-  
-  sum+=1;
-  return sum;
-  
+    double sum = 0;
+    while (count != 0) {
+        sum = sum + calcItem(x, count);
+        count--;
+    }
+    sum += 1;
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  
-  double sum = 0;
-  
-      while (count != 1) {
-        
+    double sum = 0;
+    while (count != 1) {
         sum = sum + pown(-1, count - 1) * calcItem(x, 2 * count - 1);
         count--;
     }
-  
-    sum+=x;
+    sum += x;
     return sum;
-
 }
 
 double cosn(double x, uint16_t count) {
-  
-  double sum = 0;
-  
- while(count!=1){
-   
+    double sum = 0;
+    while (count != 1) {
         sum = sum + pown(-1, count - 1) * calcItem(x, 2 * count - 2);
         count--;
-   
     }
-  
     sum += 1;
-    return sum;
-	 
-  }
+    return sum;	 
+}
